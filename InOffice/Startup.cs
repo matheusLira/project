b20334a7734cb2f30cc7c0577daa6cf168441c6e
Matheus Lira <matheus.lira.souza@gmail.com>
2019-01-06 07:45:51 -0400

    Estrutura inicial do projeto criada

---
+++ InOffice/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(InOffice.Startup))]
+namespace InOffice
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}

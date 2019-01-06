4856394475469d621ffe240c84168590130216e4
Matheus Lira <matheus.lira.souza@gmail.com>
2019-01-06 11:23:46 -0400

    Table model added

---
+++ InOffice/Controllers/HR/JobDescription/JobDescriptionController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace InOffice.Content
+{
+    public class JobDescriptionController : Controller
+    {
+        // GET: JobDescription
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
---
+++ InOffice/Controllers/HR/JobDescription/JobDescriptionListController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace InOffice.Controllers.HR.JobDescription
+{
+    public class JobDescriptionListController : Controller
+    {
+        // GET: JobDescriptionList
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
